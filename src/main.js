eefe6b493e95a6d4900d7c18fcd20bd1c57d72e0
codexu <xu461229187@qq.com>
2017-02-24 17:27:01 +0800

    .

--- src/main.js
+++ src/main.js
@@ -1,8 +1,33 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
 import App from './App'
+import goods from 'components/goods/goods.vue'
+import ratings from 'components/ratings/ratings.vue'
+import seller from 'components/seller/seller.vue'
 
-/* eslint-disable no-new */
-new Vue({
-  el: 'body',
-  components: { App }
+import 'common/scss/index.scss'
+
+Vue.use(VueRouter)
+Vue.use(VueResource)
+
+let app = Vue.extend(App)
+let router = new VueRouter({
+  linkActiveClass: 'active'
 })
+
+router.map({
+  '/goods': {
+    component: goods
+  },
+  '/ratings': {
+    component: ratings
+  },
+  '/seller': {
+    component: seller
+  }
+})
+
+router.start(app, '#app')
+
+router.go('/goods')
